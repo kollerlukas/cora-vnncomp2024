d834e0bcbb7a6827b642750f55560e81e1a91ed2
Lukas Koller <lukas.koller@tum.de>
2024-07-05 14:59:41 +0200

    handle multiple input sets

--- runCoraVnnComp24.m
+++ runCoraVnnComp24.m
@@ -17,7 +17,7 @@ options.nn = struct(...
     'poly_method','bounds',...'bounds','singh'
     'train',struct(...
         'backprop',false,...
-        'mini_batch_size',8 ...
+        'mini_batch_size',128 ...
     ) ...
 );
 % Set default training parameters
@@ -26,7 +26,7 @@ options.nn.interval_center = false;
 % Specify whether the spacial dimensions of the input have to be flipped.
 permuteInputDims = false;
 
-for i=182:size(instances,1)
+for i=185:size(instances,1)
     % Extract current instance.
     instance = instances(i,:);
     modelPath = instance.model{1};
@@ -142,63 +142,72 @@ for i=182:size(instances,1)
 
     % Load specification.
     [X0,specs] = vnnlib2cora(vnnlibPath);
+
+    for j=1:length(X0)
+
+        % Extract input set.
+        x = 1/2*(X0{j}.sup + X0{j}.inf);
+        r = 1/2*(X0{j}.sup - X0{j}.inf);
     
-    % Extract input set.
-    x = 1/2*(X0{1}.sup + X0{1}.inf);
-    r = 1/2*(X0{1}.sup - X0{1}.inf);
+        if permuteInputDims
+            inSize = nn.layers{1}.inputSize([2 1 3]);
+            x = reshape(permute(reshape(x,inSize),[2 1 3]),[],1);
+            r = reshape(permute(reshape(r,inSize),[2 1 3]),[],1);
+        end
 
-    if permuteInputDims
-        inSize = nn.layers{1}.inputSize([2 1 3]);
-        x = reshape(permute(reshape(x,inSize),[2 1 3]),[],1);
-        r = reshape(permute(reshape(r,inSize),[2 1 3]),[],1);
+        % Extract specification.
+        if isa(specs.set,'halfspace')
+            A = specs.set.c';
+            b = -specs.set.d;
+        else
+            A = specs.set.A;
+            b = -specs.set.b;
+        end
+        safeSet = strcmp(specs.type,'safeSet');
+    
+    
+        % Measure verification time.
+        tic
+    
+        % Do verification.
+        [res,x_,y_] = nn.verify(x,r,A,b,safeSet,options,timeout,true);
+    
+        if strcmp(res,'VERIFIED')
+            res = 'unsat';
+        elseif strcmp(res,'COUNTER EXAMPLE')
+            res = 'sat';
+            % Produce witness file.
+            modelName = regexp(modelPath,'(?<=.*/).*(?=(\.onnx))','match');
+            vnnlibName = regexp(vnnlibPath,'(?<=.*/).*(?=(\.vnnlib))','match');
+            witFilename = ['cora-results/' modelName{1} '_' vnnlibName{1} '.counterexample'];
+            % Open file.
+            fid = fopen(witFilename,'w');
+            % Write content.
+            fprintf(fid,'sat\n(');
+            % Write input values.
+            for j=1:size(x_,1)
+                fprintf(fid,'(X_%d %f)\n',j-1,x_(j));
+            end
+            % Write output values.
+            for j=1:size(y_,1)
+                fprintf(fid,'(Y_%d %f)\n',j-1,y_(j));
+            end
+            fprintf(fid,')');
+            % We found a counterexample; we dont have to check the other
+            % input sets.
+            break;
+        else
+            res = 'unknown';
+            % We cannot verify an input set; we dont have to check the other
+            % input sets.
+            break;
+        end
     end
-
-    % Extract specification.
-    if isa(specs.set,'halfspace')
-        A = specs.set.c';
-        b = -specs.set.d;
-    else
-        A = specs.set.A;
-        b = -specs.set.b;
-    end
-    safeSet = strcmp(specs.type,'safeSet');
-   
-
-    % Measure verification time.
-    tic
-
-    % Do verification.
-    [res,x_,y_] = nn.verify(x,r,A,b,safeSet,options,timeout,true);
-
+    % Print result.
     fprintf('%s -- %s: %s\n',modelPath,vnnlibPath,res);
     time = toc;
     fprintf('--- Verification time: %.4f / %.4f [s]\n',time,timeout);
-
-    if strcmp(res,'VERIFIED')
-        res = 'unsat';
-    elseif strcmp(res,'COUNTER EXAMPLE')
-        res = 'sat';
-        % Produce witness file.
-        modelName = regexp(modelPath,'(?<=.*/).*(?=(\.onnx))','match');
-        vnnlibName = regexp(vnnlibPath,'(?<=.*/).*(?=(\.vnnlib))','match');
-        witFilename = ['cora-results/' modelName{1} '_' vnnlibName{1} '.counterexample'];
-        % Open file.
-        fid = fopen(witFilename,'w');
-        % Write content.
-        fprintf(fid,'sat\n(');
-        % Write input values.
-        for j=1:size(x_,1)
-            fprintf(fid,'(X_%d %f)\n',j-1,x_(j));
-        end
-        % Write output values.
-        for j=1:size(y_,1)
-            fprintf(fid,'(Y_%d %f)\n',j-1,y_(j));
-        end
-        fprintf(fid,')');
-    else
-        res = 'unknown';
-    end
-
+    % Store outputs.
     benchmarkNames = [benchmarkNames; benchName];
     models = [models; modelPath];
     vnnlibs = [vnnlibs; vnnlibPath];
