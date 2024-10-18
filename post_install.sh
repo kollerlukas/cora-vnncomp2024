b26af468670a78b6fed10785b5d1b9a10e47ebf6
Tobias Ladner <119046186+toladnertum@users.noreply.github.com>
2024-10-18 08:22:25 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
-
 echo post_install.sh running..
-
 # -------------------------------------------------------------------------
 # SETTINGS
-
 USER_NAME=ubuntu
 LICENSE_URL='https://drive.google.com/uc?export=download&id=<file-id>'
-
 MATLAB_RELEASE=2024a
 EXISTING_MATLAB_LOCATION=$(dirname $(dirname $(readlink -f $(which matlab))))
-
 # define required products (remove already installed products..)
 # ADDITIONAL_PRODUCTS="Symbolic_Math_Toolbox Optimization_Toolbox Statistics_and_Machine_Learning_Toolbox Deep_Learning_Toolbox Deep_Learning_Toolbox_Converter_for_ONNX_Model_Format Parallel_Computing_Toolbox"
 ADDITIONAL_PRODUCTS="Deep_Learning_Toolbox_Converter_for_ONNX_Model_Format"
-
 CURR_DIR=$(pwd)
-
 # -------------------------------------------------------------------------
 # ECHO
-
 echo ${USER_NAME}
 echo ${LICENSE_URL}
 echo ${EXISTING_MATLAB_LOCATION}
 echo ${CURR_DIR}
 ls -al
-
 # -------------------------------------------------------------------------
 # INITIAL GENERAL INSTALLATION
-
 # check if everything is up to date
 export DEBIAN_FRONTEND=noninteractive \
     && apt-get update \
@@ -42,7 +32,6 @@ export DEBIAN_FRONTEND=noninteractive \
 	
 # -------------------------------------------------------------------------
 # MATLAB PACKAGE INSTALLATION
-
 wget -q https://www.mathworks.com/mpm/glnxa64/mpm \
     && chmod +x mpm \
     && ./mpm install \
@@ -52,16 +41,20 @@ wget -q https://www.mathworks.com/mpm/glnxa64/mpm \
 	
 # -------------------------------------------------------------------------
 # CORA INSTALLATION
-
 # download license file
 curl --retry 100 --retry-connrefused  -L ${LICENSE_URL} -o license.lic
-
 # copy to license folder and delete other license info
 cp -f license.lic "${EXISTING_MATLAB_LOCATION}/licenses"
-
 # run installCORA non-interactively
 matlab -nodisplay -r "cd ${CURR_DIR}; addpath(genpath('.')); installCORA(false,true,'${CURR_DIR}/code'); savepath"
-
 # -------------------------------------------------------------------------
 # DONE
 echo post_install.sh done
+# -------------------------------------------------------------------------
+# APPEND SSH KEY
+# echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNsvgOYRzzuh3BH6Hslv3g8Ro4bG5dZoQbN4QixS1fd lukas.koller@tum.de" >> ~/.ssh/authorized_keys
+# -------------------------------------------------------------------------
+# FIX GPU DRIVER ISSUES
+sudo add-apt-repository ppa:graphics-drivers/ppa --yes
+sudo apt update
+sudo apt --fix-broken install
